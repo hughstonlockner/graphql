66bb3501e401e0d2cd1c7b571969fe0bd3ec4bb7
Hughston Lockner <hughston94@gmail.com>
2021-04-20 11:29:45 -0400

    Add project files.

---
+++ Demo/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+}
